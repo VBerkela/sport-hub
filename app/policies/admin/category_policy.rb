265092a9ecd6387d25858908296d2ac595e0a364
v_berkela <v_berkela@ukr.net>
2021-08-19 16:58:12 +0300

    Updates after code review

--- app/policies/admin/category_policy.rb
+++ app/policies/admin/category_policy.rb
@@ -1,9 +1,9 @@
 class Admin::CategoryPolicy < ApplicationPolicy
-  def edit?
-    user.is_admin? if user
-  end
-
-  def destroy?
-    user.is_admin? if user
+  included do
+    %w(edit? destroy?).each do |method_name|
+      define_method method_name do
+        user.is_admin? if user
+      end
+    end
   end
 end
